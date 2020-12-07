d99bef67d3ebf48be9f6dafd82eb4e2807cf20a6
박진영 <nazzang49@naver.com>
2020-12-08 00:18:12 +0900

    (1207) most voted notebook 필사

--- model/dogs-vs-cats.py
+++ model/dogs-vs-cats.py
@@ -1,35 +1,167 @@
-# building powerful image classification models using very little data
-# image dataset path => C:\dogs-vs-cats
+# CNN description => http://taewan.kim/post/cnn/#fnref:1
 
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, BatchNormalization
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+import random
+import numpy as np
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
-datagen = ImageDataGenerator(
-    rotation_range=0.4,     # rotation degree
-    width_shift_range=0.2,  # lateral move horizontally
-    height_shift_range=0.2, # lateral move vertically
-    rescale=.1 / 255,       # RGB rate rescaling 0 ~ 255 => 0 ~ 1
-    shear_range=0.2,        # shearing transformation range
-    zoom_range=0.2,         # zoom and out
-    horizontal_flip=True,   # if True => flip image by 50%
-    fill_mode="nearest"     # method of filling space when "rotated" "moved" "zoomed"
+# categorize by filename
+filenames = os.listdir("C:/dogs-vs-cats/train")
+categories = []
+for filename in filenames:
+    category = filename.split('.')[0]
+    if category == 'dog':
+        categories.append(1)
+    else:
+        categories.append(0)
+
+df = pd.DataFrame({
+    'filename': filenames,
+    'category': categories
+})
+
+print(df.head())
+print(df.tail())
+df['category'].value_counts().plot.bar()
+
+# make basic CNN model
+def make_cnn_model():
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.5))
+    model.add(Dense(2, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    model.summary()
+
+    return model
+
+model = make_cnn_model()
+earlystop = EarlyStopping(patience=10)
+
+# reduce the learning rate unless accuracy increase for 2steps
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_acc',
+                                            patience=2,
+                                            verbose=1,
+                                            factor=0.5,
+                                            min_lr=0.00001)
+
+callbacks = [earlystop, learning_rate_reduction]
+df["category"] = df["category"].replace({0: 'cat', 1: 'dog'})
+
+# split train dataset
+train_df, validate_df = train_test_split(df, test_size=0.20, random_state=42)
+train_df = train_df.reset_index(drop=True)
+validate_df = validate_df.reset_index(drop=True)
+
+total_train = train_df.shape[0]
+total_validate = validate_df.shape[0]
+batch_size=15
+
+train_datagen = ImageDataGenerator(
+    rotation_range=15,
+    rescale=1./255,
+    shear_range=0.1,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    width_shift_range=0.1,
+    height_shift_range=0.1
 )
 
-# other parameters => https://keras.io/preprocessing/image
+train_generator = train_datagen.flow_from_dataframe(
+    train_df,
+    "C:/dogs-vs-cats/train",
+    x_col='filename',
+    y_col='category',
+    target_size=(128, 128),
+    class_mode='categorical',
+    batch_size=batch_size
+)
 
-# PIL
-img = load_img("C:/dogs-vs-cats/train/cats/cat.0.jpg")
-# size => 3(RGB) x 150(width) x 150(height)
-x = img_to_array(img)
-# size => 1 x 3(RGB) x 150(width) x 150(height)
-x = x.reshape((1,) + x.shape)
+validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = validation_datagen.flow_from_dataframe(
+    validate_df,
+    "C:/dogs-vs-cats/train",
+    x_col='filename',
+    y_col='category',
+    target_size=(128, 128),
+    class_mode='categorical',
+    batch_size=batch_size
+)
 
-i = 0
-for batch in datagen.flow(x,
-                          batch_size=1,
-                          save_to_dir="C:/dogs-vs-cats/train/cats-preview",
-                          save_prefix="cat",
-                          save_format="jpeg"):
-    i += 1
-    if i > 20:
-        break
+example_df = train_df.sample(n=1).reset_index(drop=True)
+example_generator = train_datagen.flow_from_dataframe(
+    example_df,
+    "C:/dogs-vs-cats/train",
+    x_col='filename',
+    y_col='category',
+    target_size=(128, 128),
+    class_mode='categorical'
+)
+
+epochs = 50
+history = model.fit_generator(
+    train_generator,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=total_validate//batch_size,
+    steps_per_epoch=total_train//batch_size,
+    callbacks=callbacks
+)
+
+model.save_weights("model.h5")
+
+# call test dataset
+test_filenames = os.listdir("C:/dogs-vs-cats/test")
+test_df = pd.DataFrame({
+    'filename': test_filenames
+})
+nb_samples = test_df.shape[0]
+
+test_gen = ImageDataGenerator(rescale=1./255)
+test_generator = test_gen.flow_from_dataframe(
+    test_df,
+    "../input/test1/test1/",
+    x_col='filename',
+    y_col=None,
+    class_mode=None,
+    target_size=(128, 128),
+    batch_size=batch_size,
+    shuffle=False
+)
+
+predict = model.predict_generator(test_generator, steps=np.ceil(nb_samples/batch_size))
+
+# pick one category comparing probability each other
+test_df['category'] = np.argmax(predict, axis=-1)
+label_map = dict((v, k) for k, v in train_generator.class_indices.items())
+
+test_df['category'] = test_df['category'].replace(label_map)
+test_df['category'] = test_df['category'].replace({'dog': 1, 'cat': 0})
+
+print(test_df['category'].value_counts())
+
 
